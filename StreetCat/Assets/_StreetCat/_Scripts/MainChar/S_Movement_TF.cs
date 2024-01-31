a57c57cd26023f62fca99578a3831e485ca3794b
Radi0Dude <tobias.fehn@hotmail.com>
2024-01-31 16:03:51 +0100

    Cat can move, collisions fixed

--- StreetCat/Assets/_StreetCat/_Scripts/MainChar/S_Movement_TF.cs
+++ StreetCat/Assets/_StreetCat/_Scripts/MainChar/S_Movement_TF.cs
@@ -1,7 +1,9 @@
 using NaughtyAttributes;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class S_Movement_TF : MonoBehaviour
 {
@@ -11,22 +13,69 @@ public class S_Movement_TF : MonoBehaviour
 	[ValidateInput("MoreThanZero", "Speed needs to be grater than zero to be able move")]
 	float speed;
 
+	float raySomething;
 	[InputAxis]
 	[SerializeField] string moveAxis;
 
-	private bool MoreThanZero()
-	{
-		return speed > 0.0f;
-	}
+	[SerializeField] Vector2 moveAmount;
+	bool collision;
 
-	
+	public float moveDir = 0;
+	float rayDir;
+	Vector3 debugRayDir;
+	Ray ray;
+	 public float horizontal;
+
+	public void OnMove(InputAction.CallbackContext callbackContext)
+	{
+		moveAmount = callbackContext.ReadValue<Vector2>();
+	}
 
 	private void Update()
 	{
-		float horizontal = Input.GetAxis(moveAxis);
+		Movement();
+	}
+
+	private void Movement()
+	{
+		horizontal = moveAmount.x;
 
 
-		Vector3 movement = new Vector3(horizontal * speed, 0, 0) * Time.deltaTime;
+		if (horizontal > 0)
+		{
+			rayDir = 0.51f;
+			transform.eulerAngles = new Vector3(0, 0, 0);
+			moveDir = horizontal;
+			debugRayDir = transform.right;
+			raySomething = 1;
+			//Maybe add animation here, get a check to see which way the animation was last frame
+		}
+		if (horizontal < 0)
+		{
+			rayDir = 0.52f;
+			transform.eulerAngles = new Vector3(0, 180, 0);
+			moveDir = -horizontal;
+			debugRayDir = transform.right;
+			raySomething = -1;
+			//Maybe add animation here, get a check to see which way the animation was last frame
+		}
+		else if (horizontal == 0)
+		{
+			moveDir = 0;
+			//Idle would go here!!
+		}
+		Debug.DrawRay(transform.position, transform.TransformDirection(debugRayDir) * 10f, Color.green);
+
+
+
+		ray = new Ray(transform.position, debugRayDir);
+
+		if (Physics.Raycast(ray, rayDir + raySomething * Time.deltaTime))
+		{
+			collision = true;
+			moveDir = 0;
+		}
+		Vector3 movement = new Vector3(moveDir * speed, 0, 0) * Time.deltaTime;
 
 		transform.Translate(movement);
 	}
