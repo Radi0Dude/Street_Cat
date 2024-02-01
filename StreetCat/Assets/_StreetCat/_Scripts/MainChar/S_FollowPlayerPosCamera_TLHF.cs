e92e4a32376a6160a88135c0d1dd048bb35d6620
Radi0Dude <tobias.fehn@hotmail.com>
2024-02-02 00:02:02 +0100

    Two player, camera works with both one and two player

--- StreetCat/Assets/_StreetCat/_Scripts/MainChar/S_FollowPlayerPosCamera_TLHF.cs
+++ StreetCat/Assets/_StreetCat/_Scripts/MainChar/S_FollowPlayerPosCamera_TLHF.cs
@@ -6,7 +6,8 @@ using UnityEngine.Rendering.PostProcessing;
 
 public class S_FollowPlayerPosCamera_TLHF : MonoBehaviour
 {
-	[SerializeField] Transform playerPos;
+	[SerializeField] Transform playerOnePos;
+	[SerializeField] Transform playerTwoPos;
 	[SerializeField] Transform pointPos;
 
 	[MinValue(3), MaxValue(10)]
@@ -21,40 +22,89 @@ public class S_FollowPlayerPosCamera_TLHF : MonoBehaviour
 
 	bool hasMultiplePlayers;
 
+	[Tag]
+	[SerializeField]
+	string pointTag;
+	[Tag]
+	[SerializeField]
+	string playertag;
+
 	private void Start()
 	{
-		OnSpawnPosition();
-		hasMoved = playerPos.position;
+		
+			playerOnePos = null;
+			playerTwoPos = null;
+			hasMultiplePlayers = false;
+
+
+	}
+
+	public void PlayerJoinedEvent()
+	{
+		pointPos = GameObject.FindGameObjectWithTag(pointTag).transform;
+		Debug.Log(GameObject.FindGameObjectsWithTag(playertag));
+		if (GameObject.FindGameObjectsWithTag(playertag).Length >= 2)
+		{
+			playerOnePos = GameObject.FindGameObjectsWithTag(playertag)[0].transform;
+			playerTwoPos = GameObject.FindGameObjectsWithTag(playertag)[1].transform;
+			hasMultiplePlayers = true;
+		}
+		else if (GameObject.FindGameObjectsWithTag(playertag).Length == 1)
+		{
+			playerOnePos = GameObject.FindGameObjectsWithTag(playertag)[0].transform;
+			playerTwoPos = null;
+			OnSpawnPositionOnePlayer();
+			hasMultiplePlayers = false;
+			hasMoved = playerOnePos.position;
+
+		}
+		else if (GameObject.FindGameObjectsWithTag(playertag).Length < 1)
+		{
+			playerOnePos = null;
+			playerTwoPos = null;
+			hasMultiplePlayers = false;
+		}
+
+
 	}
 	private void Update()
 	{
-		CalculateDistance();
+		if(!hasMultiplePlayers)
+		{
+			CalculateDistancePlayerOne();
+		}
+		else
+		{
+			CalculateDistancePlayerIfTwo();
+		}
 
 		Debug.Log(difference);	
 	}
 
-	private void OnSpawnPosition()
+	private void OnSpawnPositionOnePlayer()
 	{
-		transform.position =  new Vector3(amountToSpawnInFront + playerPos.position.x, 0, playerPos.position.z);
+		transform.position =  new Vector3(amountToSpawnInFront + playerOnePos.position.x, 0, playerOnePos.position.z);
 	}
-	private void CalculateDistance()
+	private void OnSpawnPositionTwoPlayer()
 	{
-		playerXDistance = pointPos.position.x - playerPos.position.x;
+		transform.position = (playerOnePos.position + playerTwoPos.position) / 2;
+	}
+	private void CalculateDistancePlayerOne()
+	{
+		playerXDistance = pointPos.position.x - playerOnePos.position.x;
 		cameraXDistance = pointPos.position.x - transform.position.x;
 		difference = cameraXDistance / playerXDistance;
 
-		if(hasMoved.x != playerPos.position.x)
+		if(hasMoved.x != playerOnePos.position.x)
 		{
-			float distance = playerPos.position.x - hasMoved.x;
-			transform.position = new Vector3(transform.position.x + (distance * difference), 0, playerPos.position.z);
+			float distance = playerOnePos.position.x - hasMoved.x;
+			transform.position = new Vector3(transform.position.x + (distance * difference), 0, playerOnePos.position.z);
 			Debug.Log(hasMoved);
-			hasMoved.x = playerPos.position.x;
+			hasMoved.x = playerOnePos.position.x;
 		}
-
-		
-
-
-
-		
+	}
+	private void CalculateDistancePlayerIfTwo()
+	{
+		transform.position = (playerOnePos.position + playerTwoPos.position) / 2;
 	}
 }
