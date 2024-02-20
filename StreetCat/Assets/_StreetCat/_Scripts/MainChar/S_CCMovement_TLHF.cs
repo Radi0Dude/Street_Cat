9e4446b771e33110d1a2260ebf21088051f66f1e
Radi0Dude <tobias.fehn@hotmail.com>
2024-02-20 15:12:23 +0100

    Gjprde noe animasjons greier

--- StreetCat/Assets/_StreetCat/_Scripts/MainChar/S_CCMovement_TLHF.cs
+++ StreetCat/Assets/_StreetCat/_Scripts/MainChar/S_CCMovement_TLHF.cs
@@ -1,8 +1,10 @@
 using NaughtyAttributes;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class S_CCMovement_TLHF : MonoBehaviour
 {
@@ -13,14 +15,22 @@ public class S_CCMovement_TLHF : MonoBehaviour
 	private float speed;
 	[SerializeField]
 	private Animator animator;
+	[Scene]
+	[SerializeField]
+	private string fightingSceneName;
 
+	[SerializeField]
+	private string animatorMoveName;
+	[SerializeField]
+	private string animatorFightingName;
 
 	private CharacterController controller;			
-	private Vector2 moveAmount;
+	public Vector2 moveAmount;
 	private Vector3 movement;
 	private float moveChar;
 	private float velocity;
 	private bool grounded;
+
 
 
 	private void Start()
@@ -36,6 +46,8 @@ public class S_CCMovement_TLHF : MonoBehaviour
 	{
 		grounded = controller.isGrounded;
 
+		
+
 		moveChar = moveAmount.x;
 		
 		movement = new Vector3(0, 0, moveChar * speed) * Time.deltaTime;
@@ -43,6 +55,33 @@ public class S_CCMovement_TLHF : MonoBehaviour
 		controller.Move(movement);
 
 		velocity = controller.velocity.z;
+		
+		if(animator != null )
+		{
+			animator.SetFloat(animatorMoveName, moveChar);
+		}
+
+		if(transform.position.x != 0)
+		{
+			transform.position = new Vector3(0, this.transform.position.y, this.transform.position.z);
+		}
+		//if(velocity > 0.05f)
+		//{
+		//	transform.rotation = Quaternion.Euler(0, 0, 0);
+		//}
+		//else if (velocity < 0.05f)
+		//{
+		//	transform.rotation = Quaternion.Euler(0, 180, 0);
+		//}
+	}
+
+	private void CheckState()
+	{
+		if(SceneManager.GetActiveScene().name == fightingSceneName) 
+		{
+			animator.SetBool(animatorFightingName, true);
+		}
+
 	}
 
 	private void Update()
