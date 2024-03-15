8d3d9f1e46e33017b7335c66e0f9ce774ba69361
Radi0Dude <tobias.fehn@hotmail.com>
2024-03-15 17:04:21 +0100

    Ant stuff

---
+++ StreetCat/Assets/Pitching/AntLemming/Scripts/S_Spawner_TLHF.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_Spawner_TLHF : MonoBehaviour
+{
+    GameObject spawnPrefab;
+
+	float timeSinceStarte;
+	float timeToNextSpawn;
+
+	private void Update()
+	{
+		timeSinceStarte = Time.realtimeSinceStartup;
+
+		
+	}
+}
